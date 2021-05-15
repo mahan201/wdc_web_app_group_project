b622583438af9a693fcb135c16cc0be5e4fcacfd
mahan201 <m2.noorbahr@gmail.com>
2021-05-15 07:50:14 +0000

    bunch of stuff implemented

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,27 @@
 var allTabs = ["Profile","Hotspots","Venues","Users","Signup an Admin","User History","Have I been to a Hotspot?","Email Subscription","Venue History","Venue Check-In Code"];
 
+var venueHistoryFull= [
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"}
+    ];
+
+var userHistoryFull= [
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"},
+    {firstName: "Mahan",lastName: "Noorbahr",phoneNo: "0123456789",time: "10-5-2021 13:05"}
+    ];
 
 var appdiv = new Vue({
     el: "#app",
     data: {
+        //Profile Data
         accountType: "venue",
         selectedTab: "Profile",
         editing: false,
@@ -12,8 +30,12 @@ var appdiv = new Vue({
         phoneNum: "0123456789",
         email: "abc@gmail.com",
         businessName: "McDonalds",
-        address: "123 Clown Street, Adelaide"
+        address: "123 Clown Street, Adelaide",
 
+
+        //Venue History Data
+        searchTerm: "",
+        venueCheckInCode: "MCD1231234"
     },
     computed: {
         tabs: function (){
@@ -32,11 +54,24 @@ var appdiv = new Vue({
 
             temp[allTabs.indexOf(this.selectedTab)] = true;
             return temp;
+        },
+
+        //Venue Data
+        venueHistory: function(){
+            var search = this.searchTerm;
+            var temp = [];
+            venueHistoryFull.forEach(function (checkIn) {
+               if ((checkIn.firstName.toLowerCase().includes(search.toLowerCase())) || (checkIn.lastName.toLowerCase().includes(search.toLowerCase()))){
+                   var formatted = checkIn.firstName + " " + checkIn.lastName + " " + checkIn.phoneNo + " " + checkIn.time;
+                   temp.push(formatted);
+               }
+            });
+            console.log(temp);
+            return temp;
         }
     },
     methods: {
         loadMap: function(){
-            console.log("HELLO");
                 mapboxgl.accessToken = 'pk.eyJ1IjoibWFoYW4yMDEiLCJhIjoiY2tvbXZwcmZ2MGFycjJvcG81dHFvbjI4dyJ9.YRrp93j6OerxskDUK17mWg';
                 var map = new mapboxgl.Map({
                 container: 'map',
